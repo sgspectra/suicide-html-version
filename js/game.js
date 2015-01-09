aab52293ea15cbd053bb45c6462925e3d014e51c
sgspectra <astorch13@gmail.com>
2015-01-09 15:42:59 -0500

    html and js changes

---
+++ js/game.js
@@ -0,0 +1,8 @@
+function submitPlayers(form){
+	var players = [];
+	players[0] = form.player1.value;
+	players[1] = form.player2.value;
+	players[2] = form.player3.value;
+	players[3] = form.player4.value;
+	return players;
+}
